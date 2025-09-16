5557bc92b84960332b96d4aa92dcf692a87e88a1
Pushp Ranjan Karn <prkpushp@gmail.com>
2025-09-16 11:20:18 +0530

    Create gcbdr.sh

---
+++ gcbdr.sh
@@ -0,0 +1,6 @@
+gcloud backup-dr backup-plan-associations delete plan-association-091897 --location=us-east4
+The backup plan association will be deleted. You cannot undo this action.
+
+Do you want to continue (Y/n)?  y
+
+Delete in progress for backup plan association [projects/d-ulti-lifetech-hrsd-ce8e/locations/us-east4/backupPlanAssociations/plan-association-091897] Run [backup-dr operations describe projects/d-ulti-lifetech-hrsd-ce8e/locations/us-east4/operations/operation-1758001752947-63ee4adffbaf3-0458a5b5-18de6eb5] to check the status of this operation.
