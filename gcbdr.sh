573075dbec599bc9191861f6aeed7cd6ced3ac93
Pushp Ranjan Karn <prkpushp@gmail.com>
2025-09-18 11:05:10 +0530

    Update gcbdr.sh

--- gcbdr.sh
+++ gcbdr.sh
@@ -4,3 +4,7 @@ The backup plan association will be deleted. You cannot undo this action.
 Do you want to continue (Y/n)?  y
 
 Delete in progress for backup plan association [projects/d-ulti-lifetech-hrsd-ce8e/locations/us-east4/backupPlanAssociations/plan-association-091897] Run [backup-dr operations describe projects/d-ulti-lifetech-hrsd-ce8e/locations/us-east4/operations/operation-1758001752947-63ee4adffbaf3-0458a5b5-18de6eb5] to check the status of this operation.
+
+
+Update label on GCP forwarding rule
+gcloud compute forwarding-rules update us-cfn-api-gtw-elb1-forwarding-rule --global --update-labels substream_id=c2a70bad-9106-7652-7f44-e1d6c8f1480e,env_type=cfn
